09478e14cf0036e42559275791c9bb89524ccb9b
srivatsan-asahi <srivastan@mail.asahitechnologies.com>
2021-12-03 19:30:56 +0530

    FileSharingPdfandImage

--- src/filterScreen.js
+++ src/filterScreen.js
@@ -1,45 +1,42 @@
-// React Native Camera
-// https://aboutreact.com/react-native-camera/
 
-// import React in our code
 import React, { useState, useEffect, useCallback } from 'react';
 
-// import all the components we are going to use
 import {
-    SafeAreaView,
     StyleSheet,
     Text,
     View,
-    TouchableOpacity,
     Image,
-    PermissionsAndroid,
     FlatList,
-    Platform,
-    TouchableHighlight,
+    Dimensions,
     Pressable,
+    TouchableOpacity,
 } from 'react-native';
 
+// Redux Packages
 import { connect } from 'react-redux';
-import ImagePicker from 'react-native-image-crop-picker';
-
 import { CAPTUREDIMAGE } from './redux/action';
-import { CropIcon, ImageEdit } from '../assets/icons';
+
+// Image filter Packages
 import {
     ColorMatrix,
     concatColorMatrices
 } from 'react-native-color-matrix-image-filters';
 import { negativefilter, normalfilter, greyScale } from '../assets/filters';
 
-
+// Image Converting and Sharing  Packages
+import Share from 'react-native-share';
+import RNImageToPdf from 'react-native-image-to-pdf';
+import RNFetchBlob from 'react-native-fetch-blob';
 
 
 const FilterScreen = (props) => {
+
     const { capturedImageUri, route } = props;
 
-    const [filterType, setfiltertype] = useState(normalfilter)
 
-    let uri = route.params.value.uri;
-
+    const [filterType, setfiltertype] = useState(normalfilter);
+    const [shareOption, setShareOption] = useState(false)
+    //    FilterList 
     let filterdata = [
         {
             id: 1,
@@ -63,6 +60,75 @@ const FilterScreen = (props) => {
         }
     ];
 
+    // URI 
+    let uri = route.params.value.uri;
+    let pdfuri = uri.split('file://').pop().toString()
+    // Device Width and Height
+    const deviceWidth = Dimensions.get('window').width;
+    const deviceHeight = Dimensions.get('window').height;
+
+    // PDF conversion functionality
+    function sharePDFWithAndroid(fileUrl, type) {
+        const base64Data = async (data) => {
+            const base64Data = ('data:application/pdf;base64,') + data
+            await Share.open({ url: base64Data }).then((err) => {
+                console.log(err)
+            })
+        }
+
+        RNFetchBlob.fs.readFile(fileUrl, 'base64')
+            .then(async (data) => {
+                await base64Data(data)
+
+            }).catch((err) => {
+                console.log(err)
+            });
+    }
+
+    const myAsyncPDFFunction = async () => {
+        try {
+            const options = {
+                imagePaths: [pdfuri],
+                // to convert Bin to pdf file this naming is needed
+                name: 'PDFName.pdf',
+                maxSize: { // optional maximum image dimension - larger images will be resized
+                    width: 900,
+                    height: Math.round(deviceHeight / deviceWidth * 900),
+                },
+                quality: .7, // optional compression paramter
+                targetPathRN: "/storage/emulated/0/Download/", // only for android version 9 and lower
+                //for versions higher than 9 it is stored in (Download/img-to-pdf/)
+            };
+            const pdf = await RNImageToPdf.createPDFbyImages(options);
+            sharePDFWithAndroid(pdf.filePath)
+
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    const ShareAsImage = async () => {
+
+
+        const shareOption = await Share.open(
+            {
+                url: route.params.value.uri
+            }
+        ).then((res) => {
+
+            console.log(res)
+        }).catch((err) => {
+            console.log(err)
+        });
+
+
+
+
+
+    };
+
+    // Filter List
+
     const renderItem = useCallback(({ item }) => {
         return (
 
@@ -85,11 +151,17 @@ const FilterScreen = (props) => {
 
 
 
+
+
     return (
+
         <View style={styles.container}>
-            <Pressable>
-                <Text>Share</Text>
-            </Pressable>
+            <TouchableOpacity onPress={ShareAsImage}>
+                <Text>ShareAsImage</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={myAsyncPDFFunction}>
+                <Text>ShareAsPdf</Text>
+            </TouchableOpacity>
             <ColorMatrix
                 style={{ width: '90%', height: '90%', justifyContent: 'center', alignItems: 'center' }}
                 matrix={concatColorMatrices([filterType])}
